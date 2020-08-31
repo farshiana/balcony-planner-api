c33359899b257de21d4127b4225c28dd36f06368
farshiana <alizee.farshian@gmail.com>
2020-08-31 21:08:07 +0200

    Fix create variety

--- app/controllers/varieties.controller.js
+++ app/controllers/varieties.controller.js
@@ -1,19 +1,22 @@
 import db from '../models/models';
 
-const { Variety } = db;
+const { Variety, Genus } = db;
 
 export const addVariety = async (req, res) => {
     try {
-        debugger;
+        const genus = await Genus.findByPk(req.params.genusId);
+        if (!genus) {
+            return res.status(404).send({ message: 'Genus was not found' });
+        }
 
-        const variety = await Variety.create({
+        const variety = await genus.createVariety({
             name: req.body.name,
             exposure: req.body.exposure,
             watering: req.body.watering,
         });
-        res.status(201).send(variety);
+        return res.status(201).send(variety);
     } catch (error) {
-        res.status(500).send({ message: error.message });
+        return res.status(500).send({ message: error.message });
     }
 };
 
--- app/models/genus.model.js
+++ app/models/genus.model.js
@@ -1,26 +1,37 @@
 import { CATEGORIES } from '../constants';
 
-export default (sequelize, Sequelize) => sequelize.define('genera', {
-    id: {
-        type: Sequelize.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: Sequelize.UUIDV4,
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    category: {
-        type: Sequelize.ENUM({ values: CATEGORIES }),
-        allowNull: false,
-    },
-    createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-    },
-    updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-    },
-});
+export default (sequelize, Sequelize) => {
+    const Genus = sequelize.define('genera', {
+        id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        category: {
+            type: Sequelize.ENUM({ values: CATEGORIES }),
+            allowNull: false,
+        },
+        createdAt: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+    });
+
+    Genus.associate = (models) => {
+        Genus.hasMany(models.Variety, {
+            foreignKey: 'genusId',
+            as: 'varieties',
+        });
+    };
+
+    return Genus;
+};
