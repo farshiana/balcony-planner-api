a0c151b4a184d3debebeaa70a8a76c6091aeeb7f
farshiana <alizee.farshian@gmail.com>
2020-09-02 09:22:13 +0200

    Login is PUT

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -13,5 +13,5 @@ export default (app) => {
         checkDuplicates,
         register,
         login);
-    app.post('/auth/login', validator(username, password), login);
+    app.put('/auth/login', validator(username, password), login);
 };
