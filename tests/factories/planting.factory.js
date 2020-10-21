d5343b0f58ed0bf17cbe8f2686ae6b72d4862c11
farshiana <alizee.farshian@gmail.com>
2020-10-21 19:16:28 +0200

    Update factories

--- tests/factories/planting.factory.js
+++ tests/factories/planting.factory.js
@@ -1,6 +1,8 @@
+import faker from 'faker';
 import db from '@/models/models';
 
 export default (props = {}) => db.Planting.create({
+    position: { left: faker.random.number(), top: faker.random.number() },
     seed: [0, 1],
     plant: [2, 3],
     harvest: [4, 5],
